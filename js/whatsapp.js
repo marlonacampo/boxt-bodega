9573188993f89acf2d21bc0dec234130fa6faf1f
Marlon <marlonacampo@unicesar.edu.co>
2024-02-18 11:06:21 -0500

    version1.1

---
+++ js/whatsapp.js
@@ -0,0 +1,43 @@
+function enviarWhatsApp() {
+    const title = document.querySelector('.title').textContent;
+    const price = document.querySelector('.precio span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  
+
+  function enviarWhatsApp2() {
+    const title = document.querySelector('.title2').textContent;
+    const price = document.querySelector('.precio2 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  function enviarWhatsApp3() {
+    const title = document.querySelector('.title3').textContent;
+    const price = document.querySelector('.precio3 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=+573182105866&text=${encodeURIComponent(message)}`;
+  }
+  
+  document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.keyCode === 85) {
+      e.preventDefault();
+      alert('Función deshabilitada');
+    }
+  });  
