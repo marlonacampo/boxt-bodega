f7018cb6ab38a1708294f4efd8055a7b677be331
Marlon <marlonacampo@unicesar.edu.co>
2024-02-22 10:39:41 -0500

    actualizados

--- js/whatsapp.js
+++ js/whatsapp.js
@@ -32,12 +32,116 @@ function enviarWhatsApp() {
     window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
   }
   
-  document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
-  });
-  document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.keyCode === 85) {
-      e.preventDefault();
-      alert('Función deshabilitada');
-    }
-  });  
+  function enviarWhatsApp4() {
+    const title = document.querySelector ('.title4').textContent;
+    const price = document.querySelector('.precio4 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  
+
+  function enviarWhatsApp6() {
+    const title = document.querySelector('.title5').textContent;
+    const price = document.querySelector('.precio5 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  function enviarWhatsApp6() {
+    const title = document.querySelector('.title6').textContent;
+    const price = document.querySelector('.precio6 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+  function enviarWhatsApp7() {
+    const title = document.querySelector ('.title7').textContent;
+    const price = document.querySelector('.precio7 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  
+
+  function enviarWhatsApp8() {
+    const title = document.querySelector('.title8').textContent;
+    const price = document.querySelector('.precio8 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  function enviarWhatsApp9() {
+    const title = document.querySelector('.title9').textContent;
+    const price = document.querySelector('.precio9 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+  function enviarWhatsApp10() {
+    const title = document.querySelector ('.title10').textContent;
+    const price = document.querySelector('.precio10 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  
+
+  function enviarWhatsApp11() {
+    const title = document.querySelector('.title11').textContent;
+    const price = document.querySelector('.precio11 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+
+
+  function enviarWhatsApp12() {
+    const title = document.querySelector('.title12').textContent;
+    const price = document.querySelector('.precio12 span').textContent;
+    const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+    const message = `Hola! Me gustaría comprar el producto ${title} que tiene un precio de ${price}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+    window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+  }
+  
+  
+  
+
+//
+  //function enviarWhatsApp4() {
+   // const datoMensaje = document.querySelector('.datoMensaje');
+   // const phone = document.querySelector('.btn-primary').getAttribute('data-wpp-phone');
+   // const message = ` ${datoMensaje}`;
+  
+    // Abrir WhatsApp con el mensaje predefinido
+ //   window.location.href = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+// }
